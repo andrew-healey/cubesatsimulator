88237c5904d7da611975fe2e7a3c02bf584ab87b
Sesamestrong <doolie.healey@gmail.com>
2019-02-26 18:25:47 -0500

    Added a layout and plan, complete with todos

--- index.js
+++ index.js
@@ -11,7 +11,37 @@ const controls=new OrbitControls(camera,document.getElementById("scene"));
 
 const EARTH_RADIUS;//Add something here
 
-//Define the low-poly earth as represented at mdiller/lowpoly-earth
+/**
+ * Describes the Earth class, which renders the earth, rotates it, changes resolution based on processing speeds, controls ionosonde location and sounding and utilizes the github repo at mdiller/lowpoly-earth
+ * @constructor
+ * @param period {Number} - describes the period, in seconds, of the Earth's rotation - aka day length
+ */
+function Earth(period){
+  let ret={};
+  let time=0;
+
+  /**
+   * Sets the time, both time-travelling and passing seconds automatically. Meant to be used with a slider or something similar.
+   * @param t {Number} The time, in seconds, to which to set the Cubesat's timeline.
+   */
+  function setTime(t){
+    if(typeof(t)==="undefined") t=time+1;
+    //TODO Set rotation based on time
+    time=t%period;
+  }
+
+  /**
+   * Gets the current time of the Earth - just a getter
+   */
+  function getTime(){
+    return time;
+  }
+
+  //TODO Make ionosonde control, rendering methods
+  
+  ret=Object.assign(ret,{setTime,getTime});//All public methods
+  return ret;
+}
 
 controls.enableKeys=false;
 
@@ -21,15 +51,53 @@ const CUBESAT_ALTITUDE;//Add something here
 
 //Define Cubesat stuff
 
-function Cubesat(){
+/**
+ * Describes the Cubesat class, which renders the cubesat, moves it around the sky, and allows for inspection.
+ * 
+ * @param period {Number} - describes the period, in seconds, of the Cubesat's orbit around the Earth
+ */
+function Cubesat(period){
   let ret={};
   let time=0;
+
+  /**
+   * Toggles inspection/action modes.
+   */
   function toggleInspection(){
     //...
   }
+
+  /*
+   * Sets the time, both time-travelling and passing seconds automatically. Meant to be used with a slider or something similar.
+   * @param t {Number} The time, in seconds, to which to set the Cubesat's timeline.
+   */
   function setTime(t){
     if(typeof(t)==="undefined") t=time+1;
-    //Set the spot in the orbit
-    time=t;
+    //TODO Set the spot in the orbit, maybe based on some params - customization comes later
+    time=t%period;
   }
+
+  /**
+   * Gets the current time of the Cubesat - just a getter
+   */
+  function getTime(){
+    return time;
+  }
+  
+  //TODO Add rendering methods, toggling between inspection and action mode, receiving ionosonde soundings, handling logs and sending analyzed logs to the Setting class
+
+  ret=Object.assign(ret,{getTime,setTime,toggleInspection});//All public methods
+  return ret;
+
+}
+
+/**
+ * Describes the Setting class, which handles Threejs canvas render methods and generally directs the objects in the scene
+ */
+function Setting(Three){
+  let ret={};
+  //TODO Add Threejs stuff, init stuff, control stuff, etc.
+  
+  ret=Object.assign(ret,{});
+  return ret;
 }
