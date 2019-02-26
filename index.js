1c72cd554f18b211bf85882550daf4cd3e49f2ec
Sesamestrong <doolie.healey@gmail.com>
2019-02-25 19:39:13 -0500

    Added general todos.

--- index.js
+++ index.js
@@ -6,4 +6,18 @@ const scene=new THREE.Scene();
 
 const camera=new THREE.PerspectiveCamera(window.innerWidth/window.innerHeight,1,2000);
 
+//OrbitControls, but it is possible that we don't use them - look at the low-poly Earth simulation for guidance
+const controls=new OrbitControls(camera,document.getElementById("scene"));
+
+const EARTH_RADIUS;//Add something here
+
+//Define the low-poly earth as represented at mdiller/lowpoly-earth
+
+controls.enableKeys=false;
+
+camera.position.set(-EARTH_RADIUS*1.5,0,0);
+
+const CUBESAT_ALTITUDE;//Add something here
+
+//Define Cubesat stuff
 
