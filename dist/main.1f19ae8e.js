af3518f5faeeeacef8090550583341cb53035653
foxxes <tomotsmith@gmail.com>
2019-04-12 20:03:00 -0400

    commit

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -44412,7 +44412,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57883" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59025" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
